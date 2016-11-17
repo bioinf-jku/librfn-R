34ed587f5feea4a10b96a01d123ef19fb8d1384a
Balazs Bencze <megette_a@yahoo.com>
2016-11-17 15:41:05 +0100

    remove print

--- src/librfn.cpp
+++ src/librfn.cpp
@@ -200,7 +200,6 @@ int train(XTypeConst X_host, float* W_host, float* P_host, const int n, const in
             printf("gemm\n");
             printf("n %d, m %d, k %d, lda %d, ldb %d\n", k, batch_size, m, m, m);
             op.printm("Wout", Wout, m, k);
-            op.printm("H before", H, k, batch_size);
 
             // REMOVE FILL
             op.fill(H, k * batch_size, 0);
