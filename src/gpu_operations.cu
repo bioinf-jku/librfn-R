328dbd65c9afb2d5711dcb3ea13df9b52c82abee
Balazs Bencze <megette_a@yahoo.com>
2016-11-18 17:19:44 +0100

    fix some stuff

--- src/gpu_operations.cu
+++ src/gpu_operations.cu
@@ -481,6 +481,7 @@ void GPU_Operations::gemm(const char *transa, const char *transb, const int m, c
 	CUSPARSE_CALL(cusparseScsrmm2(cusparse_handle, opA, opB, row_major_a->m, n, ncol_a,
 			row_major_a->nnz, &alpha, descr, row_major_a->values, row_major_a->rowPointers, row_major_a->columns, b, ldb, &beta, c, ldc));
 
+	free(row_major_a->values);
 	free(row_major_a->columns);
 	free(row_major_a->rowPointers);
 	std::free(row_major_a);
@@ -540,6 +541,7 @@ void GPU_Operations::gemm(const char *transa, const char *transb, const int m, c
 	default_stream();
 
 	if (opB != CUSPARSE_OPERATION_NON_TRANSPOSE) {
+		free(row_major_a->values);
 		free(b_trans->columns);
 		free(b_trans->rowPointers);
 	}
